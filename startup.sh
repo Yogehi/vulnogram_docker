2ed59691d11001397f5e180f363fa1c7cc7fbb3a
Ken Gannon <yogehi@gmail.com>
2020-12-24 12:31:35 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,5 @@
+echo "====================================="
+echo "== F-Secure Vulnogram Docker Image =="
+echo "== Browse to http://localhost:3555 =="
+echo "====================================="
+python3 -m http.server 3555
